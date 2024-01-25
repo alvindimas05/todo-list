bc6d16be148e2d4432bcbc8e2a6e6b22f824049a
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2024-01-25 09:23:08 +0700

    relationship

--- migrations/todos.sql
+++ migrations/todos.sql
@@ -1,5 +1,7 @@
 CREATE TABLE `todos` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `title` VARCHAR(255) NOT NULL,
-    `time` TIME NOT NULl
+    `time` TIME NOT NULl,
+    `user_id` CHAR(36) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 )
